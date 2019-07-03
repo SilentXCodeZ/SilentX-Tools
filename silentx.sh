3c52cc869c5189a400979dc55ca72367894bbd5b
SilentXCodeZ <52485272+SilentXCodeZ@users.noreply.github.com>
2019-07-03 15:25:07 +0700

    Create silentx.sh

---
+++ silentx.sh
@@ -0,0 +1,54 @@
+
+pkg install figlet
+clear
+figlet SilentX 
+
+echo "\033[35;1m × Created By Muhammad Randika R ×"
+sleep 1
+echo "\033[34;1m SilentX Tools"
+sleep 1
+echo "\033[91;1m Follow My Facebook : Randika Kys"
+sleep 1
+echo "\033[33;1m - Tools Facebook -"
+echo "\033[36;1m1.""\033[36;1mGenerator Access Token Facebook"
+echo "\033[36;1m2.""\033[36;1mGenerator Id Account Facebook"
+echo "\033[36;1m3.""\033[36;1mUnable Profile Guard Facebook"
+sleep 1
+echo "\033[33;1m - Tools BOM SMS -"
+echo "\033[36;1m4.""\033[36;1mBOM SMS TELKOMSEL"
+echo "\033[36;1m5.""\033[36;1mBOM SMS TRI"
+echo "\033[36;1m6.""\033[36;1mBOM SMS PHD"
+echo "\033[36;1m7.""\033[36;1mBOM TELEPON TOKOPEDIA"
+sleep 1
+echo "\033[33;1m - Tools WEBSITE -"
+echo "\033[36;1m8.""\033[36;1mENCODE BASE64"
+echo "\033[36;1m9.""\033[36;1mDECODE BASE64"
+echo "\033[31;1m Pilih Tools Yang Ingin Anda Gunakan:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+echo "\033[31;1mBahasa Pemograman:""\033[37;1mPHP"
+echo "\033[31;1mPembuat:""\033[37;1mMuhammad Randika R"
+sleep 2
+clear
+php fb1.php
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+echo "\033[31;1mBahasa Pemograman:""\033[37;1mPHP"
+echo "\033[31;1mPembuat:""\033[37;1mMuhammad Randika R"
+sleep 2
+clear
+php fb2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+echo "\033[31;1mBahasa Pemograman:""\033[37;1mPHP"
+echo "\033[31;1mPembuat:""\033[37;1mMuhammad Randika R"
+sleep 
+clear
+php fb3.php
+fi
